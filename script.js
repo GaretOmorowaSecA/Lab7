535d98352f909f7a7c567ad841cb5d1481a38888
GaretOmorowaSecA <N01698537@humber.ca>
2024-06-28 16:34:47 -0400

    task list
    added an output for task list
    that showcase the task created
    just one list tho :)

--- script.js
+++ script.js
@@ -21,8 +21,19 @@ function addTask() {
 
 function finishTasks() {
     const taskList = document.getElementById('taskList');
+    const outputList = document.getElementById('outputList');
     const confirmNewList = confirm("Your TO-DO LIST HAS BEEN SAVED. Create a new one?");
+    
     if (confirmNewList) {
+       
+        outputList.innerHTML = '';
+        Array.from(taskList.children).forEach((taskItem, index) => {
+            const outputItem = document.createElement('div');
+            outputItem.className = 'output-item';
+            outputItem.innerText = `${index + 1}. ${taskItem.innerText.slice(taskItem.innerText.indexOf('.') + 2)}`;
+            outputList.appendChild(outputItem);
+        });
+        
         taskList.innerHTML = ''; 
         document.getElementById('taskInput').focus(); 
     }
